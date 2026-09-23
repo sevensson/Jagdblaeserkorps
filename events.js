c796a3430c7d574582d124adc12e54df6a9da0bd
sevensson <tkassebohm@googlemail.com>
2026-09-23 16:06:17 +0200

    Create events.js

---
+++ events.js
@@ -0,0 +1,28 @@
+/* =========================================================
+   JAGDBLÄSERCORPS HEGERING RADEVORMWALD
+   VERANSTALTUNGEN
+   ========================================================= */
+
+const events = [
+
+  {
+    title: "Übungsabend",
+    date: "2026-09-23",
+    time: "18:00",
+    meetingTime: null,
+    location: "Radevormwald",
+    clothing: null,
+    description: "Gemeinsame Probe des Jagdbläsercorps."
+  },
+
+  {
+    title: "Hubertusmesse",
+    date: "2026-11-08",
+    time: "18:00",
+    meetingTime: "17:00",
+    location: "Radevormwald - Remlingrade Kirche",
+    clothing: "Jagdliche Kleidung",
+    description: "Musikalische Begleitung der Hubertusmesse."
+  }
+
+];
