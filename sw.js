ec67646aa3ab081057e00282cb2dc097df38317b
sevensson <tkassebohm@googlemail.com>
2026-09-24 13:49:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,36 @@
-const CACHE_NAME = "jagdblaeser-v2";
+/* =========================================================
+   JAGDBLÄSERCORPS HEGERING RADEVORMWALD
+   SERVICE WORKER
+   ========================================================= */
+
+
+const CACHE_NAME = "jagdblaeser-v3";
+
 
 const APP_FILES = [
+
   "./",
+
   "./index.html",
+
   "./styles.css",
+
   "./app.js",
+
+  "./events.js",
+
+  "./music-library.js",
+
   "./manifest.json",
+
   "./images/logo.png",
+
   "./images/icon-192.png",
+
   "./images/icon-512.png"
+
 ];
+
 
 
 /* =========================================================
@@ -17,21 +38,41 @@ const APP_FILES = [
    Grunddateien der App speichern
    ========================================================= */
 
-self.addEventListener("install", (event) => {
 
-  event.waitUntil(
+self.addEventListener(
+  "install",
+  (event) => {
 
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(APP_FILES);
-      })
 
-  );
+    event.waitUntil(
 
-  self.skipWaiting();
+      caches
+        .open(
+          CACHE_NAME
+        )
 
-});
+        .then(
+          (cache) => {
+
+            return cache.addAll(
+              APP_FILES
+            );
+
+          }
+
+        )
+
+    );
+
+
+    self.skipWaiting();
+
+
+  }
+
+);
+
+
 
 
 /* =========================================================
@@ -39,75 +80,163 @@ self.addEventListener("install", (event) => {
    Alte Cache-Versionen entfernen
    ========================================================= */
 
-self.addEventListener("activate", (event) => {
 
-  event.waitUntil(
+self.addEventListener(
+  "activate",
+  (event) => {
 
-    caches
-      .keys()
-      .then((cacheNames) => {
 
-        return Promise.all(
+    event.waitUntil(
 
-          cacheNames
-            .filter((cacheName) => {
-              return cacheName !== CACHE_NAME;
-            })
-            .map((cacheName) => {
-              return caches.delete(cacheName);
-            })
 
-        );
+      caches
+        .keys()
 
-      })
-      .then(() => self.clients.claim())
+        .then(
+          (cacheNames) => {
 
-  );
 
-});
+            return Promise.all(
+
+
+              cacheNames
+
+                .filter(
+                  (cacheName) => {
+
+                    return (
+                      cacheName !== CACHE_NAME
+                    );
+
+                  }
+
+                )
+
+                .map(
+                  (cacheName) => {
+
+                    return caches.delete(
+                      cacheName
+                    );
+
+                  }
+
+                )
+
+
+            );
+
+
+          }
+
+        )
+
+
+        .then(
+          () => {
+
+            return self.clients.claim();
+
+          }
+
+        )
+
+
+    );
+
+
+  }
+
+);
+
+
 
 
 /* =========================================================
    DATEIEN LADEN
-   Netzwerk bevorzugen, Cache als Rückfallebene
+   Netzwerk bevorzugt
+   Cache als Rückfallebene
    ========================================================= */
 
-self.addEventListener("fetch", (event) => {
 
-  if (event.request.method !== "GET") {
-    return;
+self.addEventListener(
+  "fetch",
+  (event) => {
+
+
+    if (
+      event.request.method !== "GET"
+    ) {
+
+      return;
+
+    }
+
+
+
+    event.respondWith(
+
+
+      fetch(
+        event.request
+      )
+
+
+      .then(
+        (response) => {
+
+
+          const responseCopy =
+            response.clone();
+
+
+
+          caches
+            .open(
+              CACHE_NAME
+            )
+
+            .then(
+              (cache) => {
+
+
+                cache.put(
+                  event.request,
+                  responseCopy
+                );
+
+
+              }
+
+            );
+
+
+
+          return response;
+
+
+        }
+
+      )
+
+
+      .catch(
+        () => {
+
+
+          return caches.match(
+            event.request
+          );
+
+
+        }
+
+      )
+
+
+    );
+
+
   }
 
-  event.respondWith(
-
-    fetch(event.request)
-
-      .then((response) => {
-
-        const responseCopy =
-          response.clone();
-
-        caches
-          .open(CACHE_NAME)
-          .then((cache) => {
-            cache.put(
-              event.request,
-              responseCopy
-            );
-          });
-
-        return response;
-
-      })
-
-      .catch(() => {
-
-        return caches.match(
-          event.request
-        );
-
-      })
-
-  );
-
-});
+);
